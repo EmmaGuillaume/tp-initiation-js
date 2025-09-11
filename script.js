db9c886649190cd4a9b3fd3299bff263a2644698
EmmaGuillaume <emma.guillaume@mmibordeaux.com>
2025-09-11 11:29:06 +0200

    tp5

--- script.js
+++ script.js
@@ -7,3 +7,20 @@ let nomMois = 'Septembre';
 let isWinter = false;
 
 console.log( nomJour, numJour, nomMois, isWinter)
+
+let tabJours = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"];
+let tabMois = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Aout", "Septembre", "Octobre", "Novembre", "Décembre"];
+
+console.log(tabJours[3], tabMois[8]);
+
+let maDateV1 = {
+	"jour" : tabJours[3],
+	"mois" : tabMois[8]
+};
+
+let maDateV2 = {
+	"jour" : tabJours[4],
+	"mois" : tabMois[10]
+}
+
+console.log(maDateV1.jour, maDateV2.mois);
