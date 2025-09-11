71a3183ec0cf072ba7385c94ca1d4354d5d72de9
EmmaGuillaume <emma.guillaume@mmibordeaux.com>
2025-09-11 11:14:21 +0200

    tp4

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-let reponse = prompt('Quel est ton meme préférée ?');
-alert('Ton meme est ' + reponse);
-            
+// let reponse = prompt('Quel est ton meme préférée ?');
+// alert('Ton meme est ' + reponse);
+
+let nomJour = 'Jeudi';
+let numJour = 11;
+let nomMois = 'Septembre';
+let isWinter = false;
+
+console.log( nomJour, numJour, nomMois, isWinter)
