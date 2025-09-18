e10d02002d6f1b3ea2df263e6a9266477f015fe8
EmmaGuillaume <emma.guillaume@mmibordeaux.com>
2025-09-18 09:31:06 +0200

    tp8

---
+++ monscript.js
@@ -0,0 +1,13 @@
+let number = 5;
+let text = '5';
+let isRainingToday = true;
+
+if (number == text){
+	console.log("double égale")
+}
+if (number === text){
+	console.log("triple monstre")
+}
+if (isRainingToday){
+	console.log(isRainingToday)
+}
