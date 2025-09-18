2b64f0061d6e87a3c86d5a1507d767eb523e18ab
EmmaGuillaume <emma.guillaume@mmibordeaux.com>
2025-09-18 11:03:04 +0200

    tp11

--- monscript.js
+++ monscript.js
@@ -12,8 +12,8 @@
 // 	console.log(isRainingToday)
 // }
 
-let age = Number(prompt("Ton age"));
-console.log(age);
+// let age = Number(prompt("Ton age"));
+// console.log(age);
 
 // if (age < 18) {
 //   console.log("T'es mineur");
@@ -29,21 +29,35 @@ console.log(age);
 //   console.log("centenaire");
 // }
 
-switch (age) {
-  case 18:
-    console.log("il vient d’être majeur");
-    break;
-  case 25:
-    console.log("quart siecle");
-    break;
-  case 50:
-    console.log("demi siecle");
-    break;
-  case 62:
-    console.log("retraite");
-    break;
-  case 100:
-    console.log("centenaire");
-    break;
-  default:
+// switch (age) {
+//   case 18:
+//     console.log("il vient d’être majeur");
+//     break;
+//   case 25:
+//     console.log("quart siecle");
+//     break;
+//   case 50:
+//     console.log("demi siecle");
+//     break;
+//   case 62:
+//     console.log("retraite");
+//     break;
+//   case 100:
+//     console.log("centenaire");
+//     break;
+//   default:
+// }
+
+
+
+let i =0;
+let j =0;
+
+while (i<=100){
+	
+	j +=i;
+	i++;
+	
+	console.log(i,j);
 }
+console.log(j)
