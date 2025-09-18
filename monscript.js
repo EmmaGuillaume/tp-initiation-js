9dfc717cb86c35f76e2508049d32a008689f2a9f
EmmaGuillaume <emma.guillaume@mmibordeaux.com>
2025-09-18 09:36:30 +0200

    tp 9

--- monscript.js
+++ monscript.js
@@ -1,13 +1,26 @@
-let number = 5;
-let text = '5';
-let isRainingToday = true;
+// let number = 5;
+// let text = '5';
+// let isRainingToday = true;
 
-if (number == text){
-	console.log("double égale")
+// if (number == text){
+// 	console.log("double égale")
+// }
+// if (number === text){
+// 	console.log("triple monstre")
+// }
+// if (isRainingToday){
+// 	console.log(isRainingToday)
+// }
+
+
+let age = prompt('Ton age');
+
+if (age <18){
+	console.log("T'es mineur")
 }
-if (number === text){
-	console.log("triple monstre")
+else if(age >18 && age<62){
+	console.log("T'es majeur")
 }
-if (isRainingToday){
-	console.log(isRainingToday)
+else {
+	console.log("T'es majeur et retraité")
 }
