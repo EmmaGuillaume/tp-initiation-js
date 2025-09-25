f6401a0afac8a931214350cc26f640d1dae8c02a
EmmaGuillaume <emma.guillaume@mmibordeaux.com>
2025-09-25 10:48:39 +0200

    tp 15

--- monscript.js
+++ monscript.js
@@ -1,5 +1,9 @@
 /* Toutes les variables */
 let li = document.querySelector("#listeCourses li");
+let dateJour = new Date(Date.now());
+let dateJourFr = dateJour.toLocaleDateString('fr-FR');
+let h2 = document.querySelector("h2");
+h2.innerHTML = h2.innerHTML+" : " + dateJourFr;
 
 /* Tous les évènements */
 li.addEventListener("click", (e) => {
